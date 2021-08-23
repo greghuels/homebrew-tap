f778a57f13587730dbc5e8419be3cd99fa85cd6f
Greg Huels <greg.huels@gmail.com>
2021-08-23 04:32:30 -0500

    Add bottles

--- Formula/git-fury.rb
+++ Formula/git-fury.rb
@@ -5,7 +5,12 @@ class GitFury < Formula
   sha256 "04eeadb62d2f59c300e067adbc00687cda609b3b0521129cdf7752351e54cb61"
   license "MIT"
 
-  depends_on "deno"
+  bottle do
+    sha256 cellar: :any_skip_relocation, catalina:     "24eaa517c28c4b7a74050c30efd10d06b9437e8af2c49184c5cfdbf9d3bcb19a"
+    sha256                               x86_64_linux: "1d5cd03c0585da0a820e4cb5d928675c2d21c1a3fdd69bbaaf625d78e29245bb"
+  end
+
+  depends_on "deno" => :build
 
   def install
     system "deno", "compile", "--allow-run", "--output=git-fury", "./mod.ts"
