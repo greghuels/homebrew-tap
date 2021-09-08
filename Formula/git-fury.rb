2ebd65951c5d4c8e84bf418486f425fc4a12e9a8
Greg Huels <greg.huels@gmail.com>
2021-09-08 08:44:37 -0500

    git-fury-1.4.0
    Closes #4.
    
    Signed-off-by: Greg Huels <1248641+greghuels@users.noreply.github.com>

--- Formula/git-fury.rb
+++ Formula/git-fury.rb
@@ -1,16 +1,9 @@
 class GitFury < Formula
   desc "Enables shorthand git syntax and allows users to add notes to branches"
   homepage "https://github.com/greghuels/git-fury"
-  url "https://github.com/greghuels/git-fury/archive/refs/tags/1.3.0.tar.gz"
-  sha256 "04eeadb62d2f59c300e067adbc00687cda609b3b0521129cdf7752351e54cb61"
+  url "https://github.com/greghuels/git-fury/archive/refs/tags/1.4.0.tar.gz"
+  sha256 "73231c4f5000e4a96b368d800cd1b13af76831a8a05a40440304019893031cc3"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/greghuels/homebrew-tap/releases/download/git-fury-1.3.0"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, catalina:     "24eaa517c28c4b7a74050c30efd10d06b9437e8af2c49184c5cfdbf9d3bcb19a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1d5cd03c0585da0a820e4cb5d928675c2d21c1a3fdd69bbaaf625d78e29245bb"
-  end
 
   depends_on "deno" => :build
 
