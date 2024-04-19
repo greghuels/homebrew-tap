6cf4cd112fc10a36c995c78fd64d102729a93ee6
Greg Huels <1248641+greghuels@users.noreply.github.com>
2024-04-19 15:03:53 +0000

    git-fury: add 1.4.3 bottle.

--- Formula/git-fury.rb
+++ Formula/git-fury.rb
@@ -5,6 +5,12 @@ class GitFury < Formula
   sha256 "8bcf9552b5a6c0916fbc53f02b65ad2e398a11d1e49c731cadbe969755f82b09"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/greghuels/homebrew-tap/releases/download/git-fury-1.4.3"
+    sha256 cellar: :any_skip_relocation, monterey:     "4e5560d7a335087bbcea2bcb0e0d4ceff5ec29061dce681fa55c93195a373109"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "aed011f83bbaa948284269399bc78667305c09f9d12bcc815f45d50881c79cb7"
+  end
+
   depends_on "deno" => :build
 
   def install
